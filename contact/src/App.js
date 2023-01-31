272a348e1564c2c2ef7e1c0ed309690d5a0ca320
markcfleming <markcfleming1990@gmail.com>
2023-01-31 08:15:00 -0500

    added search form

--- contact/src/App.js
+++ contact/src/App.js
@@ -2,12 +2,14 @@
 import "./App.css";
 import Nav from "./components/Nav/nav.js";
 import Form from "./components/Form/form.js";
+import Search from "./components/Search/search.js";
 
 function App() {
   return (
     <div>
       <Nav />
       <Form />
+      <Search />
     </div>
   );
 }
