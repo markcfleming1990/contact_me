a333966fd0dbd8edf1bdef8a4cc60bd0ca9f648c
markcfleming <markcfleming1990@gmail.com>
2023-02-01 12:04:31 -0500

    cleaning up Apollo code

--- contact/src/App.js
+++ contact/src/App.js
@@ -1,8 +1,14 @@
-// import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowswerRouter as Router, Routes, Route } from "react-router-dom";
+import { ApolloClient, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import Nav from "./components/Nav/nav.js";
 import Form from "./components/Form/form.js";
 import Search from "./components/Search/search.js";
+
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
 
 function App() {
   return (
